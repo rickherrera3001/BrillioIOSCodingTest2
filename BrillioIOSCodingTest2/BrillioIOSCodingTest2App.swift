ae36970a69a929231b211d41a9ea95154d605b56
rickherrera3001 <rihr.3001@gmail.com>
2024-04-16 15:15:58 -0600

    Initial Commit

---
+++ BrillioIOSCodingTest2/BrillioIOSCodingTest2App.swift
@@ -0,0 +1,17 @@
+//
+//  BrillioIOSCodingTest2App.swift
+//  BrillioIOSCodingTest2
+//
+//  Created by Ricardo Developer on 16/04/24.
+//
+
+import SwiftUI
+
+@main
+struct BrillioIOSCodingTest2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrillioIOSCodingTest2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BrillioIOSCodingTest2
+//
+//  Created by Ricardo Developer on 16/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
